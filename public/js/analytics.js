652d6d48e1fd86f97d9024698381a3ea0ee1b85b
Luis Osnaya <osnet@outlook.com>
2016-03-18 11:16:17 -0600

    Se repara el error que tenia en las etiquetas meta para visualizacion en explorer.

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-75150756-1', 'auto');
+ga('create', 'UA-53550714-1', 'auto');
 ga('send', 'pageview');
